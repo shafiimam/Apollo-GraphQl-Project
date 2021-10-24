574a3da91629502b9b6f8ec7351a05385fb4e630
shafiimam <shafi15-8072@diu.edu.bd>
2021-10-24 17:40:39 +0600

    started files added

---
+++ frontend/pages/products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductPage = () => {
+   return (
+      <div>
+         hello this is products frrom app
+      </div>
+   );
+};
+
+export default ProductPage;
